9683eeecc62ea215f75ef8ba43f9dc9c8ecdd0f0
Ryan Hird <ryan.hird@bennetts.co.uk>
2017-02-01 17:27:01 +0000

    Refactor and remove non-static implementation

---
+++ src/SwashbuckleAspNetVersioningShim/SetVersionInPathsDocumentFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SwashbuckleAspNetVersioningShim
+{
+    public class SetVersionInPathsDocumentFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            swaggerDoc.Paths = swaggerDoc.Paths.ToDictionary(
+                path => path.Key.Replace("v{version}", string.Format($"v{swaggerDoc.Info.Version}")),
+                path => path.Value
+                );
+        }
+    }
+}
