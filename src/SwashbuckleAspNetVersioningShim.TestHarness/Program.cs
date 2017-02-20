46dd6cbc4fdde49382a6702a049916fc1856189d
Ryan Hird <ryan.hird@bennetts.co.uk>
2017-02-20 13:03:55 +0000

    Remove AppInsights from test harness

--- src/SwashbuckleAspNetVersioningShim.TestHarness/Program.cs
+++ src/SwashbuckleAspNetVersioningShim.TestHarness/Program.cs
@@ -13,7 +13,6 @@ namespace SwashbuckleAspNetVersioningShim.TestHarness
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
